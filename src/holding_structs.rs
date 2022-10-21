cb514672f26ca3d820d59b22232cb517a2e8aa96
Gregory Hess <gregory.w.hess@gmail.com>
2022-10-20 17:07:58 -0700

    added module

---
+++ src/holding_structs.rs
@@ -0,0 +1,194 @@
+use math::round;
+
+struct AbilityScore{
+    score: i64,
+    name: String,
+}
+impl AbilityScore{
+    pub fn get_score(&self)->i64{
+        self.score
+    }
+    pub fn get_name(&self)->&String{
+        &self.name
+    }
+    pub fn get_modifier(&self)->i64{
+        self.score/2 -5
+    }
+    pub fn new(name: String, score: i64)->AbilityScore{
+        AbilityScore { score, name}
+    }
+}
+enum Profficiency {
+    None,
+    Half,
+    Profficient,
+    Expert,
+}
+struct Skill<'a>{
+    base_score: &'a AbilityScore,
+    name: String,
+    prof_rank: Profficiency
+}
+impl Skill<'_>{
+    pub fn get_prof(&self)->&Profficiency{
+        &self.prof_rank
+    }
+    pub fn get_mod(&self,prof_bonus: i64)->i64{
+        self.base_score.get_modifier()+match self.get_prof(){
+            Profficiency::None => 0,
+            Profficiency::Half => prof_bonus/2,
+            Profficiency::Profficient => prof_bonus,
+            Profficiency::Expert => 2*prof_bonus,
+        }
+    }
+    pub fn new<'a>(name: String, base_score: &'a AbilityScore, prof_rank: Profficiency)->Skill<'a>{
+        Skill {base_score, name, prof_rank}
+    }
+}
+struct Class{
+    name: String,
+    level: i64,
+}
+impl Class{
+    pub fn get_name(&self)->&String{
+        &self.name
+    }
+    pub fn get_level(&self)->i64{
+        self.level
+    }
+    pub fn new(name: String, level: i64)->Class{
+        Class { name, level}
+    }
+}
+struct Background{
+    name: String,
+    description: String,
+}
+impl Background{
+    pub fn get_name(&self)->&String{
+        &self.name
+    }
+    pub fn get_description(&self)->&String{
+        &self.description
+    }
+    pub fn new(name: String, description: String)-> Background{
+        Background { name, description }
+    }
+}
+struct Die{
+    size: i64,
+    num: i64,
+}
+impl Die{
+    pub fn get_size(&self)->i64{
+        self.size
+    }
+    pub fn get_num(&self)->i64{
+        self.num
+    }
+    pub fn to_string(&self)->String{
+        format!("{}d{}",self.num,self.size).to_string()
+    }
+    pub fn new(size: i64,num: i64)->Die{
+        Die { size, num }
+    }
+}
+enum AtkBonus{
+    Bonus(i64),
+    DC(i64),
+}
+impl AtkBonus{
+    pub fn to_string(&self)->String{
+        match self{
+            AtkBonus::Bonus(k)=>{
+                if k>&0{
+                    format!("+{}",k).to_string()
+                } else {
+                    format!("{}",k).to_string()
+                }
+            },
+            AtkBonus::DC(k) =>{
+                format!("DC {}",k).to_string()
+            }
+        }
+    }
+}
+struct Attack{
+    name: String,
+    bonus: AtkBonus,
+    damage: String
+}
+impl Attack{
+    pub fn get_name(&self)->&String{
+        &self.name
+    }
+    pub fn get_bonus(&self)->&AtkBonus{
+        &self.bonus
+    }
+    pub fn get_bonus_as_string(&self)->String{
+        self.bonus.to_string()
+    }
+    pub fn get_damage(&self)->&String{
+        &self.damage
+    }
+    pub fn new(name: String,bonus: AtkBonus,damage: String)->Attack{
+        Attack { name , bonus, damage }
+    }
+}
+struct Item{
+    quantity: i64,
+    name: String,
+}
+impl Item{
+    pub fn get_quantity(&self)->i64{
+        self.quantity
+    }
+    pub fn get_name(&self)->&String{
+        &self.name
+    }
+    pub fn new(quantity: i64,name: String)->Item{
+        Item { quantity, name}
+    }
+}
+struct SpellLevel{
+    level: i64,
+    spells: Vec<String>
+}
+impl SpellLevel{
+    pub fn get_lvl(&self)->i64{
+        self.level
+    }
+    pub fn get_spells(&self)->&Vec<String>{
+        &self.spells
+    }
+    pub fn new(level: i64, spells: Vec<String>)->SpellLevel{
+        SpellLevel { level, spells }
+    }
+}
+type SpellList = Vec<SpellLevel>;
+struct Character<'a>{
+    pub ability_scores: Vec<AbilityScore>,
+    pub saving_throws: Vec<Skill<'a>>,
+    pub skills: Vec<Skill<'a>>,
+    pub name: String,
+    pub prof_bonus: i64,
+    pub classes: Vec<Class>,
+    pub background: Background,
+    pub race: String,
+    pub alignment: String,
+    pub xp: i64,
+    pub hp: i64,
+    pub hit_dice: Vec<Die>,
+    pub attacks: Vec<Attack>,
+    pub other_profs: Vec<String>,
+    pub traits: String,
+    pub ideals: String,
+    pub bonds: String,
+    pub flaws: String,
+    pub equipment: Vec<Item>,
+    pub equiped: Vec<Item>,
+    pub coins: (i64,i64,i64,i64,i64),
+    pub features: Vec<String>,
+    pub spell_lists: Vec<SpellList>,
+    pub spell_slots: (i64,i64,i64,i64,i64,i64,i64,i64,i64)
+}
